5c7abb6a542537bfdb9628d816f33f2561f71495
sajju <suhailmd.s18@gmail.com>
2023-09-13 11:37:03 +0530

    second commit

--- hello.java
+++ hello.java
@@ -1,7 +1,11 @@
-class hello
+import java.io.*;
+public class sajju
 {
 public static void main(String[] args)
 {
 System.out.println("hello,world from git");
+System.out.println("welcome to cloyud computing");
+System.out.println("change afyter first commit");
+
 }
 }
